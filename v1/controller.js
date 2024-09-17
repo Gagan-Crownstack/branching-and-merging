262b388250c671f0088b8147548eb9f24886ff57
Gagan-Crownstack <gagan@crownstack.com>
2024-09-17 18:14:30 +0530

    v1_publisher_added

--- v1/controller.js
+++ v1/controller.js
@@ -1,3 +1,10 @@
 export const fetchall=(req,res)=>{
     res.status(200).json({message:"welcome to v1 API"})
 }
+
+export const fetchData=(req,res)=>{
+    res.status(200).json({data:{
+        version_name:1.01,
+        publush_by:"gagan Prakash"
+    }})
+}
--- v1/route.js
+++ v1/route.js
@@ -1,9 +1,11 @@
 import express from 'express'
-import { fetchall } from './controller.js';
+import { fetchall,fetchData } from './controller.js';
 
 const routerv1 = express.Router();
 
 
 routerv1.get('/',fetchall);
+routerv1.get('/data',fetchData);
+
 
 export default routerv1;
