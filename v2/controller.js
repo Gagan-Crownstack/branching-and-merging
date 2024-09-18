786d9bffa7b8f9d25850febcd9f2dfb5ab228d3a
Gagan-Crownstack <gagan@crownstack.com>
2024-09-18 13:00:41 +0530

    feature c added in v2

--- v2/controller.js
+++ v2/controller.js
@@ -11,18 +11,3 @@ export const getdata=(req,res)=>{
 export const getUpdates=(req,res)=>{
     res.status(200).json({updates: "This new update is for version 2 and the application is more secure than previously"})
 }
-
-export const featureA=(req,res)=>{
-    res.status(200).json({
-        name:"FeatureA",
-        des:"this will divide the work load equally",
-        
-    })
-}
-
-export const featureB =(req,res)=>{
-    res.status(200).json({
-        name:"FeatureB",
-        des:"This app is use AI to track user money"
-    })
-}
