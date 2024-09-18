b276954428d92cec1a20d46ad17aa79984d3ff08
Gagan-Crownstack <gagan@crownstack.com>
2024-09-18 12:11:34 +0530

    features added

--- v2/route.js
+++ v2/route.js
@@ -7,5 +7,7 @@ const routerv2 = express.Router();
 routerv2.get('/',getall);
 routerv2.get('/data',getdata);
 routerv2.get('/update',getUpdates);
+routerv2.get('/feature/a',featuresA);
+routerv2.get('/feature/b',featureB);
 
 export default routerv2;
