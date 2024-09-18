2aebe6d135dcd295c0e39649a97027b5d9a9fa3a
Gagan-Crownstack <gagan@crownstack.com>
2024-09-18 10:38:01 +0530

    v202

--- v2/controller.js
+++ v2/controller.js
@@ -1,3 +1,9 @@
 export const getall=(req,res)=>{
     res.status.json({message:"Welcome to v2"});
 }
+export const getdata=(req,res)=>{
+    res.status(200).json({
+        v2:"2.01",
+        publisher:"Gagan"
+    })
+}
